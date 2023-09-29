35f43caa05e2d7cbc5ad3f38f7246daa505b57c7
LeonPuchinger <lepu1590@gmail.com>
2023-09-29 16:59:30 +0200

    install missing toolchain components

--- build.sh
+++ build.sh
@@ -12,11 +12,22 @@ if [[ -z $DEVICE ]]; then
     echo "Set the DEVICE environment variable to use a different one."
 fi
 
+# Build toolchain is fetched at runtime, so the image always uses the latest sources
+
+echo "fetching build sources and toolchain..."
+
 git clone --depth 1 -b ${KERNEL_VERSION} https://github.com/radxa/kernel.git
 git clone --depth 1 -b master https://github.com/radxa/rkbin.git
 git clone --depth 1 -b debian https://github.com/radxa/build.git
+curl -o gcc-arm-x86_64-aarch64-none-linux-gnu.tar.gz https://dl.radxa.com/tools/linux/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu.tar.gz
+tar zxvf gcc-arm-x86_64-aarch64-none-linux-gnu.tar.gz -C /usr/local/
+cd /usr/local/
+export PATH="$(find . -name aarch64-none-linux-gnu-ld -type f -print0 | xargs -0 dirname | head -n 1 | xargs -0 realpath -z):$PATH"
+cd -
 
 echo "starting kernel build..."
 ./build/mk-kernel.sh ${DEVICE}
 
+ls -la
+ls -la /
 ls out/kernel/
