2bb047cfb241aa4e9b8bc7de2e770e726d36eb25
LeonPuchinger <lepu1590@gmail.com>
2023-09-29 12:50:04 +0200

    pull build sources

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "hello, docker!"
+if [[ -z $KERNEL_VERSION ]]; then
+    KERNEL_VERSION="linux-5.10-gen-rkr3.4"
+    echo "Kernel version not explicitly set, using default: ${KERNEL_VERSION}"
+    echo "Set the KERNEL_VERSION environment variable to use a different one."
+fi
+
+git clone -b ${KERNEL_VERSION} https://github.com/radxa/kernel.git
+git clone -b master https://github.com/radxa/rkbin.git
+git clone -b debian https://github.com/radxa/build.git
