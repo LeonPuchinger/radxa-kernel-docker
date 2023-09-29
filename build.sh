83d046888e3261f0f9f92bd9c7898b439e06f5c2
LeonPuchinger <lepu1590@gmail.com>
2023-09-29 12:57:01 +0200

    start kernel build

--- build.sh
+++ build.sh
@@ -6,6 +6,17 @@ if [[ -z $KERNEL_VERSION ]]; then
     echo "Set the KERNEL_VERSION environment variable to use a different one."
 fi
 
+if [[ -z $DEVICE ]]; then
+    DEVICE="rk3588-rock-5b"
+    echo "DEVICE not explicitly set, using default: ${DEVICE}"
+    echo "Set the DEVICE environment variable to use a different one."
+fi
+
 git clone -b ${KERNEL_VERSION} https://github.com/radxa/kernel.git
 git clone -b master https://github.com/radxa/rkbin.git
 git clone -b debian https://github.com/radxa/build.git
+
+echo "starting kernel build..."
+./build/mk-kernel.sh ${DEVICE}
+
+ls out/kernel/
